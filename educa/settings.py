e141f69966af95fe167db8e2f0eebf5f8004c9af
FiveMachine <tlee07299@gmail.com>
2023-10-18 23:15:40 -0400

    e-learning

--- educa/settings.py
+++ educa/settings.py
@@ -122,3 +122,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
